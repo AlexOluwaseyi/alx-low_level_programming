9683ca9a7983ec94bb74c3793fdaf771846aa77a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-16 02:22:32 -0800

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char STRING[64] = "Programming is like building a multilingual puzzle.";
+	char STRING[64] = "\"Programming is like building a multilingual puzzle";
 
 	puts(STRING);
 

3f321aa8493f3be0b8b7712038afd02ff842fa56
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-16 02:30:04 -0800

    Added description to function main() in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char STRING[64] = "\"Programming is like building a multilingual puzzle";

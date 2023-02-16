0c20f0b0c7f65e8625afb20c3a24cec02d371a9e
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-16 04:36:57 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(msg) - 1;
 	ssize_t ret = write(2, msg, len);
 
-	return (ret=1);
+	return (ret = 1);
 
 }
 

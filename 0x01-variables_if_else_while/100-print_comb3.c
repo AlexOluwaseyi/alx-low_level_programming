4f4998a2e55d053d223aee0db8fc67916f5cc175
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-17 14:33:02 -0800

    Double digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,11 @@ int main(void)
 			{
 				putchar(x);
 				putchar(y);
-				putchar(',');
-				putchar(' ');
+				if (x != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}

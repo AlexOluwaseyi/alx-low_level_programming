7bbeef6a40b212eaa63987f9e1007a948b837ade
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-21 05:18:42 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Program that prints _putchar
+ * followed by a newline.
+ *
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for 0x02-functions_nested_loops

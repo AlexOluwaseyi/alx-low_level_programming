806fa8ab20575413a4fa1340eec9193c71be052e
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-21 17:32:16 -0800

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints minute of Jack Bauer day starting from
+ * 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			_putchar(':');
+			if (j < 10)
+			{
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+
+}
+

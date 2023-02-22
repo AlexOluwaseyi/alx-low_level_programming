cded2bc7ba007353f9920e41daee64c012be5a57
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-22 08:14:52 -0800

    Print times table

--- 0x02-functions_nested_loops/100-times_table2.c
+++ 0x02-functions_nested_loops/100-times_table2.c
@@ -4,22 +4,9 @@
  * print_times_table - prints the multiplication table from 0 to 9
  * @n: Limit of multiplication table
  */
-void output3()
-{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
-}
-void output2()
-{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-}
 void print_times_table(int n)
 {
-	int i, j, prod;
+	int i, j, prod, x;
 
 	if ((n < 0) || (n > 15))
 	{
@@ -36,7 +23,10 @@ void print_times_table(int n)
 					_putchar(prod + '0');
 					for (j = 0; j <= (n - 1); j++)
 					{
-						output3();
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
 						_putchar(prod + '0');
 					}
 				}
@@ -44,21 +34,27 @@ void print_times_table(int n)
 					_putchar(prod + '0');
 				else if (prod < 10)
 				{
-					output3();
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(prod + '0');
 				}
 				else if ((prod >= 10) && (prod < 100))
 				{
-					output2();
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
 				}
 				else if (prod >= 100)
 				{
+					x = prod / 10;
 					_putchar(',');
 					_putchar(' ');
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10) % 10) + '0');
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
 					_putchar((prod % 10) + '0');
 				}
 			}

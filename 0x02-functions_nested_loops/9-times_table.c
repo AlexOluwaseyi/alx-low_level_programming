ab8abdf8d3b71f15aaf5233d3ef91216a361f44b
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-21 19:00:51 -0800

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,19 @@ void times_table(void)
 		{
 			prod = i;
 			prod *= j;
-			if (prod == 0)
+			if (i == 0)
 			{
 				_putchar(prod + '0');
+				for (j = 0; j <= 8; j++)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
 			}
+			else if (prod == 0)
+				_putchar(prod + '0');
 			else if (prod < 10)
 			{
 				_putchar(',');

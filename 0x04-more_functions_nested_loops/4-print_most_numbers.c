e617f411b229e9b0039a8d8fbd9735a2858499d3
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-23 09:36:48 -0800

    Okay

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
-
 void print_most_numbers(void)
 {
-	int i = 0;
+int c;
 
-	for (i = 0; i <= 9; i++)
-	{
-		if ((i != 2) && (i != 4))
-		{
-			_putchar(i + '0');
-		}
-	}
-	_putchar('\n');
+for (c = 0; c <= 9; c++)
+{
+if (c != 2)
+{
+if (c != 4)
+{
+_putchar(c + '0');
+}
+}
+}
+_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	more_numbers();
+	return (0);
+}

f13abaa6b60b2fa84e7794efc3f1bf0f2a60a7e6
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-23 18:35:10 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints a fizz-buzz from 1 to 100
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf(" FizzBuzz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i == 1)
+			printf("%d", i);
+		else 
+			printf(" %d", i);
+	}
+	putchar('\n');
+	return (0);
+}

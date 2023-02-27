ba51848f0237ec79a10cc6f948f9f5ea2ea548b8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-02-27 14:25:38 -0800

    Print Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string
+ * @s: the string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		_putchar(s[i-1]);
+		i--;
+	}
+	_putchar('\n');
+}

60686f8e951113a21bc8a1d391c2c15e5ce3a399
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-01 12:37:22 -0800

    Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string to a destination
+ * @dest: desination string
+ * @src: source string
+ * @n: maximum number of bytes to by copies from source to destination
+ * Return: destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+

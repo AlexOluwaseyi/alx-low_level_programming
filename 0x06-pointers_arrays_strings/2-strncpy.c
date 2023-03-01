4513c1ea2d2183883e53f149a70154727b344977
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-01 12:50:46 -0800

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,4 +10,22 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int srclen = 0;
+	int destlen = 0;
+	int i = 0;
 
+	while (src[srclen] != '\0')
+	{
+		srclen++;
+	}
+	while (dest[destlen] != '\0')
+	{
+		destlen++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[destlen] = '\0';
+	return (dest);
+}

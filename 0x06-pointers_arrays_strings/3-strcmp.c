49752c85af514da0802b1c46c99abfb611949084
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-01 13:58:23 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int diff = 0;
+
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	else
+	{
+		while (s1[i] == s2[i])
+		{
+			i++;
+		}
+		diff = s1[i] - s2[i];
+	}
+	return (diff);
+}

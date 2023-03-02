ba79c390a3501515129663730bbe67343c1724c1
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-01 16:40:24 -0800

    TOUPPERCASE

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Convert lowercase characters to uppercase
+ * @n: string with lowercase characters
+ */
+
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = (n[i] - 'a') + 'A';
+		}
+		i++;
+	}
+	return (n);
+}

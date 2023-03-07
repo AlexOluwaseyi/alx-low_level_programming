7766e5195a7bd5206285f1350ccd87c5bb82c651
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-06 17:07:56 -0800

    Chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 
 
 

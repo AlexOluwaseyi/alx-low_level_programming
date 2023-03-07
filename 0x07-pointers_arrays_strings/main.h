0ccbabbddb7cf80d0e4b981102484aa77b44dbb0
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-06 17:02:21 -0800

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 
 

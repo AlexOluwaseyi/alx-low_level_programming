6ca9fe256b194834fc8c49868eb3936f3a39d911
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-10 08:16:40 -0800

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,15 @@ int main(int argc, char *argv[])
 	int product;
 
 	i += argc;
-	product = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", product);
+	if (argc == 3)
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }

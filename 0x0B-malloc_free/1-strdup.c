ecf885a8262e8b9affaa11a825f9e96e4db6d3a8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-13 12:24:38 -0700

    _strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - Copies a string to a pointer
+ * @str: string to be copied to pointer
+ *
+ * Return: NULL if str = NULL or insufficient memory
+ * On success, rturns a pointer to duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int len = 0;
+	char *dupl;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	dupl = malloc(sizeof(char) * len);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		dupl[i] = str[i];
+	}
+	return (dupl);
+}

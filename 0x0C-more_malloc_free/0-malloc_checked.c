5ee790c042fe17617bc30a627526724e36569612
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-16 08:18:32 -0700

    Malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,9 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: pointer to address
+ *
+ * Return: pointer to allocated memory
+ * Or cause normal process termination if malloc fails.
  */
 
 void *malloc_checked(unsigned int b)

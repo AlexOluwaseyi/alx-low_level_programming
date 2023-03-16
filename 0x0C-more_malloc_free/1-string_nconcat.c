c61fd1a87ce72efa2d85396af158e7c6e2cdeff8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-16 09:37:43 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,22 +14,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *newString;
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
-	unsigned int i = 0;
-	unsigned int x, y, sum;
+	unsigned int x, sum;
 
 	if (s1 == NULL)
-		s1 = "";
+		len1 = 0;
 	else
 	{
-		while (s1[len1] != '\0')
-			len1++;
+		for (len1 = 0; s1[len1]; len1++)
+			;
 	}
 	if (s2 == NULL)
-		s2 = "";
+		len2 = 0;
 	else
 	{
-		while (s2[len2] != '\0')
-			len2++;
+		for (len2 = 0; s2[len2]; len2++)
+			;
 	}
 	if (n < len2)
 		len2 = n;
@@ -39,11 +38,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (x = 0; x < len1; x++)
 		newString[x] = s1[x];
-	for (y = len1; y < len1 + len2; y++)
+	for (x = 0; x < len2; x++)
 	{
-		newString[y] = s2[i];
-		i++;
+		newString[x + len1] = s2[x];
 	}
-	newString[x + y] = '\0';
+	newString[len1 + len2] = '\0';
 	return (newString);
 }

ee103c01b9871365938a18d4810b3a20bad60869
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-16 17:31:49 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == old_size)
-		return (int *)(ptr);
+		return (ptr);
 	if (new_size > old_size)
 		ptr = realloc(ptr, new_size);
 	if (ptr == NULL)

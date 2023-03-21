e224ac46df94c13bec39a754e688e596dc99c38a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-20 21:23:16 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+*/
+
 struct dog
 {
 	char *name;
@@ -9,5 +17,7 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 
 #endif /* ifndef DOG_H */

50a2daa3820588b78345f172735868334e268359
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-21 04:45:59 -0700

    Free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

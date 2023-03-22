c25597cc14e49dee12f99a0cfedf8f71c7332408
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-22 14:23:25 -0700

    Fixed Betty Issues

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,8 +2,10 @@
 
 /**
  * array_iterator - executes a function on each element of an array
+ * @array: the array to be iterated
  * @size: Size of the array.
  * @action: pointer to the function to be used
+ * Return: void.
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))

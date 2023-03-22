77843ee0a0be8358fc38822f4ce7d8f0662868bf
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-22 14:42:29 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,13 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 		{
-			return(i);
+			return (i);
 		}
 	}
 	return (-1);

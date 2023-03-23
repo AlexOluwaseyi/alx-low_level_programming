9d15b43e69ada9e99ba073fd16c09497ebd00a20
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-03-23 16:18:03 -0700

    Opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,17 +14,22 @@ int main(int argc, char *argv[])
 	char *p;
 	int size;
 
-	if (argc != 2 || argv[1][0] == '-')
+	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 	size = atoi(argv[1]);
-
+	if (size < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	p = (char *)main;
 	for (i = 0; i < size; i++)
 	{
 		printf("%02x ", *(p + i) & 0xff);
 	}
 	printf("\n");
+	return (0);
 }

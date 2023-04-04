db4a1db983d481c563bf84a884a0fef09f0c29b7
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-04-04 14:20:24 -0700

    Print lists elements

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+/**
+ * struct list - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: Singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif /* ifndef LISTS_H */

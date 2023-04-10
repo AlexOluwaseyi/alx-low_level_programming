df5a4a5f934280cb90934ccc167218739f80b7eb
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-04-10 14:44:44 -0700

    Flip bits counter

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,10 +11,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int x = n ^ m;
 	unsigned int count = 0;
+
 	while (x != 0)
 	{
 		count += x & 1;
-		x >>=1;
+		x >>= 1;
 	}
 	return (count);
 }

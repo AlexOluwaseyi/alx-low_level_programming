3fb79305263473bce4a09c7df43638f2a9667e9c
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-04-10 09:11:45 -0700

    Binary to Unsgined int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /* #ifndef MAIN_H */

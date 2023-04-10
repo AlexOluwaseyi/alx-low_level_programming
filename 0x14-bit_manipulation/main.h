0a59b3488490d49e7461f2ded7da91a97187af21
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-04-10 10:03:51 -0700

    Decimal-Binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,8 +7,9 @@
 #include <math.h>
 #include <ctype.h>
 
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 
 #endif /* #ifndef MAIN_H */

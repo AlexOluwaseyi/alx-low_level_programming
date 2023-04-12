89b69524649db68d7260bb92b43b2c994cc80388
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-04-12 09:54:07 -0700

    Requiescat

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+/* Function prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif /* #ifndef MAIN_H */

81bf5e9ea790aaad75cc42e6042e5eb794ce9b14
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-04-12 11:00:42 -0700

    Under the Snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <malloc.h>
 
 
 /* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 
 
 #endif /* #ifndef MAIN_H */

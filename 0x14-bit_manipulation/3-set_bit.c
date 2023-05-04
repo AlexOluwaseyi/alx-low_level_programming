3e502adc6641201ec4069221720b6358a2b1f5b3
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-05-04 14:06:33 -0700

    Set bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - sets bit at index to 1
+ * @n: integer to be converted to decimal
+ * @index: index at which bit is to be chaged to 1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	if (index > (sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+
+	mask <<= index;
+	*n |= mask;
+	return (1);
+}

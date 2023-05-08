b24a99846a282a04e82c0db273c77f3d008e06ca
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-05-08 00:25:53 -0700

    Extra effort

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,30 +3,17 @@
 /**
  * print_binary - converts decimal to binary
  * @n: decimal integer to be converted
- * Return: binary form of integer
+ * Return: no return value
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 2147483648;
-	int bitCount = 0;
-	int i;
-
-	for (i = 0; i < 32; i++)
+	if (n >> 0)
 	{
-		if ((n & mask) != 0)
-		{
-			bitCount = 1;
-			_putchar('1');
-		}
-		else if (bitCount == 1)
-		{
-			_putchar('0');
-		}
-		n <<= 1;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	if (bitCount == 0)
-	{
+	else
 		_putchar('0');
-	}
 }

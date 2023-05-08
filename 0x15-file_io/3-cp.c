09a45f03bc1a5d4909867a99b51c6d04c244c2b8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-05-08 04:03:46 -0700

    File I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#define BUFSIZE 1024
+
+/**
+ * main - entry point function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0, Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, len;
+	char buffer[BUFSIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((len = read(fd_from, buffer, BUFSIZE)))
+	{
+		if (len == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (write(fd_to, buffer, len) != len)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	return (0);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+This project is all about File I/O
+ALX SWE

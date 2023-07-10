df22328fe2764974d29d177656311349622545b4
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-07-10 11:57:36 -0700

    Update: Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,30 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fp = fopen(filename, "w+");
-	if (fp == NULL)
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-		fwrite(text_content, sizeof(char), strlen(text_content), fp);
-	fclose(fp);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
 }

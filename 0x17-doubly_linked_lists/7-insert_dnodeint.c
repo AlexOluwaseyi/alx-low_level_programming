02c490999749b9af1fd53eb9599270b018a9798d
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-18 00:35:32 -0700

    Updated code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp_node;
+
 	unsigned int i;
 
 	if (h == NULL)
@@ -32,9 +33,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 	temp_node = *h;
-
 	for (i = 0; i < idx - 1 && temp_node != NULL; i++)
 		temp_node = temp_node->next;
+	if (temp_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = temp_node->next;
 	temp_node->next = new_node;
 	return (new_node);

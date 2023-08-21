52072a9f9e52f909b506f0922467d354769a1d10
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-08-21 04:04:23 -0700

    Calling a C Dynamic Library in Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+
+/**
+ * add - add two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: sum of the two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: difference between two integers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: division result of the two integers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Finds the modulo of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: sum of the two integers
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
